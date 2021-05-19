5527ad66fcba97d3b09b033841f62fdee2505ab7
shodgetts <stephenhodgetts91@gmail.com>
2021-05-19 17:06:10 -0600

    pushy pushy remove keys

--- methods/client-side/Events/ListenToPublicChannel.js
+++ methods/client-side/Events/ListenToPublicChannel.js
@@ -7,7 +7,7 @@ const StreamChat = require("stream-chat").StreamChat;
 const test = async () => {
   const client = new StreamChat(
     "dz5f4d5kzrue",
-    "msp77tpbfcmmv98pk598jvvu93rygd3nexctp7c74tgyfsfd3ub8d9dg9gtz6a6s",
+    "secret",
     { timeout: 6000 }
   );
 
