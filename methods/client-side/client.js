05e0cbe0f2fdc12e0dcba536aa96b0747ca0dad9
Todd Norton <toddmnorton@live.com>
2021-05-20 08:55:15 -0600

    committing

--- methods/client-side/client.js
+++ methods/client-side/client.js
@@ -1,4 +1,5 @@
-require("dotenv").config({ path: __dirname + "/.env" });
+// require("dotenv").config({ path: __dirname + "/.env" });
+require('dotenv').config();
 
 const app_key = process.env["APP_KEY"];
 const secret = process.env["API_SECRET"];
